cdc5e06f34290bfd9e43ea41bce569a6e12d014e
Arjeta Ademi <arjetaademi47@gmail.com>
2025-08-16 00:58:32 +0200

    first commit

---
+++ src/components/SocialLogin.jsx
@@ -0,0 +1,20 @@
+ const SocialLogin =() => {
+    return (
+<div className="social-login">
+        <button className="social-button">
+          <img src="/images/google.svg" alt="Google"
+           className="social-icon" />
+          Google
+        </button>
+
+        <button className="social-button">
+          <img src="/images/apple.svg" alt="Apple" 
+          className="social-icon" />
+          Apple
+        </button>
+      </div>
+
+    )
+ }
+
+ export default SocialLogin
